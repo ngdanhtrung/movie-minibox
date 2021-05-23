20d50c0861275e24f518c3e5574b5276d1f85ea7
TRẦN LÊ CHÁNH HẠNH <4501104069@student.hcmue.edu.vn>
2021-05-23 15:13:14 +0700

    edited header.php

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,20 +1,23 @@
-<div>
-    <div class="container">
-            <div>
-                <h3>CÔNG TY MINI BOX VIỆT NAM</h3>
-                <p>Địa Chỉ: Số 280 An Dương Vương, Phường 4, Quận 5, Thành phố Hồ Chí Minh</p>
-                <p>Hotline: 028.38352020 - 38352021 / Fax: 8428.38398946</p>
-                <p>COPYRIGHT 2021 MINI BOX. All RIGHTS RESERVED .</p>
-            </div>
+</div>
+<div id="footer">
+    <div class="footer-container">
+        <div class="footer-text-container">
+            <h3>CÔNG TY MINI BOX VIỆT NAM</h3>
+            <p>Địa Chỉ: Số 280 An Dương Vương, Phường 4, Quận 5, Thành phố Hồ Chí Minh</p>
+            <p>Hotline: 028.38352020 - 38352021 / Fax: 8428.38398946</p>
+            <p>COPYRIGHT 2021 MINI BOX. All RIGHTS RESERVED .</p>
+        </div>
     </div>
     <div style="
-    height:114px;
-    width: 100%;
-    background: url(https://media.discordapp.net/attachments/845814665542893584/845847302627524608/bg-bottom-footer.jpg);
-    background-repeat: repeat-x;
-    ">
+        height:114px;
+        width: 100%;
+        background: url(https://media.discordapp.net/attachments/845814665542893584/845847302627524608/bg-bottom-footer.jpg);
+        background-repeat: repeat-x;
+        ">
+    </div>
 </div>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 </body>
+
 </html>
--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -7,40 +7,42 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
   <link rel="stylesheet" href="./css/header.css">
+  <link rel="stylesheet" href="./css/footer.css">
   <title>Mini Box | Thông tin - Lịch Chiếu - Hệ Thống rạp chiếu phim đỉnh cao</title>
 </head>
 
 <body>
-  <div id="header">
-    <div class="header-text">
-      <a href="">Vé của tôi</a>
-      <a href="">Đăng nhập</a>
-      <a href="">Đăng ký</a>
-    </div>
-    <div class="header-page">
-      <div class="header-page-container">
-        <a class="logo" href="/"><img class="logo" src="./img/comm7.png" alt="logo"></a>
-        <div class="dropdown-box">
-          <button class="dropbtn">Phim
-            <div class="dropdown-content">
-              <a href="#">phim đang chiếu</a>
-              <a href="#">phim sắp chiếu</a>
-            </div>
-          </button>
-          <button class="dropbtn">Rạp
-            <div class="dropdown-content">
-              <a href="#">tất cả rạp</a>
-            </div>
-          </button>
-          <button class="dropbtn">Thành viên
-            <div class="dropdown-content">
-              <a href="#">tài khoản</a>
-              <a href="#">quyền lợi</a>
-            </div>
-          </button>
-          <a href="/about" class="dropbtn">giới thiệu</a>
+  <div id="wrapper">
+    <div id="header">
+      <div class="header-text">
+        <a href="">Vé của tôi</a>
+        <a href="">Đăng nhập</a>
+        <a href="">Đăng ký</a>
+      </div>
+      <div class="header-page">
+        <div class="header-page-container">
+          <a class="logo" href="/"><img class="logo" src="./img/comm7.png" alt="logo"></a>
+          <div class="dropdown-box">
+            <button class="dropbtn">Phim
+              <div class="dropdown-content">
+                <a href="#">phim đang chiếu</a>
+                <a href="#">phim sắp chiếu</a>
+              </div>
+            </button>
+            <button class="dropbtn">Rạp
+              <div class="dropdown-content">
+                <a href="#">tất cả rạp</a>
+              </div>
+            </button>
+            <button class="dropbtn">Thành viên
+              <div class="dropdown-content">
+                <a href="#">tài khoản</a>
+                <a href="#">quyền lợi</a>
+              </div>
+            </button>
+            <a href="/about" class="dropbtn">giới thiệu</a>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-<div style="background: #FDFCF0">
+    <div style="background: #FDFCF0">
