de741202f55adbc366d6a079dd250a0b90342ccf
NGUYỄN DANH TRUNG <4501104260@student.hcmue.edu.vn>
2021-05-23 09:53:45 +0700

    Added header footer and about page

---
+++ app/Views/templates/header.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mini Box | Thông tin - Lịch Chiếu - Hệ Thống rạp chiếu phim đỉnh cao</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+</head>
+<body>
+
+<section>
+<nav class="navbar navbar-light" 
+style="
+background: url(https://media.discordapp.net/attachments/845814665542893584/845830559921995786/bg-top.png);
+height: 79px;
+">
+  <div class="container-fluid" 
+  style="
+  justify-content:center;
+  font-family: sans-serif;
+  font-weight: bold;
+  font-size: 24px;
+  width: 80%;
+  "
+  >
+    <div style="width:10%">
+      <a class="navbar-brand" href="/">
+        <img src="https://cdn.discordapp.com/attachments/845814665542893584/845814923988435004/cgvlogo-small.png" alt="" width="68" height="30">
+      </a>
+    </div>
+    <div style="width:60%">
+      <ul class="nav justify-content-center">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color:#222222">
+            PHIM
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Phim Đang Chiếu</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Phim Sắp Chiếu</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color:#222222">
+            RẠP
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Tất Cả Các Rạp</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Rạp Đặc Biệt</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Rạp Rạp Sắp Mở</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color:#222222">
+            THÀNH VIÊN
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Tài Khoản</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Quyền Lợi</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/about" style ="color: #222222">Giới Thiệu</a>
+        </li>
+      </ul>
+    </div>
+    <div style="width=10%">
+      <form class="container-fluid justify-content-start">
+        <button class="btn btn-outline-success me-2" type="button">Đăng Nhập</button>
+        <button class="btn btn-sm btn-outline-secondary" type="button">Đăng Ký</button>
+      </form>
+    </div>
+  </div>
+</nav>
+</section>
+<div style="background: #FDFCF0">
