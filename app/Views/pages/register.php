0f47f1c9edcede8ace8c589f360adf3707964955
NGUYỄN DANH TRUNG <4501104260@student.hcmue.edu.vn>
2021-05-27 15:17:25 +0700

    Add event slider in login and register form

--- app/Views/pages/register.php
+++ app/Views/pages/register.php
@@ -1,6 +1,6 @@
 <div class="container">
   <div class="row mt-5">
-    <div class="col-sm-7 p-5" style="color: #636363">
+    <div class="col-sm-6 p-5" style="color: #636363">
       <h3>Đăng Ký</h3>
       <hr>
       <form class="" action="/account/register" method="post">
@@ -46,6 +46,29 @@
         </div>
         <button type="submit" class="btn btn-danger col-12 mt-2">Đăng ký</button>
       </form>
+    </div>
+    <div class="col-sm-6 p-5" style="color: #636363">
+        <div id="carouselExampleFade" class="carousel slide carousel-fade carousel-dark" data-bs-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="<?php echo base_url() ?>./img/event-1.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="<?php echo base_url() ?>./img/event-2.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="<?php echo base_url() ?>./img/event-3.jpg" class="d-block w-100" alt="...">
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
     </div>
   </div>
 </div>
