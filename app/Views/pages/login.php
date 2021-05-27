c6292ca6ff3816ab94ec61cc7d60a79c250b53ac
TRẦN LÊ CHÁNH HẠNH <4501104069@student.hcmue.edu.vn>
2021-05-27 15:12:07 +0700

    added now-showing, coming-soon, user page WIP

--- app/Views/pages/login.php
+++ app/Views/pages/login.php
@@ -1,6 +1,7 @@
 <div class="container">
-  <div class="row mt-5">
-    <div class="col-sm-7 p-5" style="color: #636363">
+  <div class="row mt-2">
+    <div class="col-sm-1"></div>
+    <div class="col-sm-6 p-5" style="color: #636363">
       <h3>Đăng nhập</h3>
       <hr>
       <?php if (session()->get('success')) : ?>
@@ -9,21 +10,21 @@
         </div>
       <?php endif; ?>
       <form class="" action="/account/login" method="post">
-        <div class="form-group">
-          <label for="username">Tên đăng nhập</label>
-          <input type="text" class="form-control" name="username" id="username" value="<?= set_value('username') ?>">
-        </div>
-        <div class="form-group">
-          <label for="password">Mật khẩu</label>
-          <input type="password" class="form-control" name="password" id="password" value="">
-        </div>
-        <?php if (isset($validation)) : ?>
-          <div class="col-12">
-            <div class="alert alert-danger" role="alert">
+        <div class="row">
+          <div class="form-group">
+            <label for="username">Tên đăng nhập: </label>
+            <input type="text" class="form-control" name="username" id="username" value="<?= set_value('username') ?>">
+          </div>
+          <div class="form-group">
+            <label for="password">Mật khẩu: </label>
+            <input type="password" class="form-control" name="password" id="password" value="">
+          </div>
+          <?php if (isset($validation)) : ?>
+            <div class="alert m-auto text-danger" role="alert">
               <?= $validation->listErrors() ?>
             </div>
-          </div>
-        <?php endif; ?>
+          <?php endif; ?>
+        </div>
         <div class="row">
           <div class="col-12 col-sm-4">
           </div>
